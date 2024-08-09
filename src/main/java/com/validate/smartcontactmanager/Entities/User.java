d3726bfa3416c501fa0dc9bf118c296d19cc4ffc
salmankn <modsalmankhan1@gmail.com>
2024-08-10 01:50:06 +0530

    this is controlling every single handler

---
+++ src/main/java/com/validate/smartcontactmanager/Entities/User.java
@@ -0,0 +1,125 @@
+package com.validate.smartcontactmanager.Entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "USER")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotBlank(message = "Name filed is required !!")
+    @Size(min = 3, message = "min 2 and max 20 characters are allowed")
+    private String name;
+    @Column(unique = true)
+    @Pattern(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+$", message = "Invalid email !")
+    //@Email(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+$", message = "Invalid email !") we can apply both but recommended @Pattern
+    private String email;
+    //@Transient // meaning it will not be saved in DB
+    @Size(min = 5, max = 15, message = "password must be field!")
+    private String password;
+    private String role;
+    private boolean enabled;
+    private String imageUrl;
+    @Column(length = 500)
+    private String about;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Contact> contacts = new ArrayList<>();
+
+    public User() {
+        super();
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                ", enabled=" + enabled +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", about='" + about + '\'' +
+                ", contacts=" + contacts +
+                '}';
+    }
+}
